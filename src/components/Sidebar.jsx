aa862db92cc38ae1e8b455ef6817b28bcb849ddd
Tamires Calixto <tamires.americo.calixto@gmail.com>
2023-06-29 13:41:03 -0300

    react_sass-main

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+import SocialNetworkContainer from "./SocialNetworkContainer";
+import InformationContainer from "./InformationContainer";
+
+import Avatar from "../img/eu.jpg";
+
+import "../styles/components/sidebar.sass";
+
+const Sidebar = () => {
+  return (
+    <aside id="sidebar">
+      <img src={Avatar} alt="Tamires Calixto" />
+      <p className="title">Desenvolvedora</p>
+      <SocialNetworkContainer />
+      <InformationContainer />
+      <a href="#" className="btn">
+        Download currículo
+      </a>
+    </aside>
+  );
+};
+
+export default Sidebar;
